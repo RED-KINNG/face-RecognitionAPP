ac0e3e18df85ef81c9c52f3c20ba23f3a7e9d557
Red Kinng <68705440+RED-KINNG@users.noreply.github.com>
2020-08-28 15:46:07 -0700

    Updates

---
+++ precache-manifest.d0b3250670234f8b0b180570c91eb578.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f928f7937a331df274c9af250ecf4d7",
+    "url": "/RED-KINNG/facerecognizerAPP.git/index.html"
+  },
+  {
+    "revision": "de7a22ad4893f773a90d",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6d468e390a9d7702af63",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/css/main.ebbb795d.chunk.css"
+  },
+  {
+    "revision": "de7a22ad4893f773a90d",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/js/2.1d0d399f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/js/2.1d0d399f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d468e390a9d7702af63",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/js/main.eb20d091.chunk.js"
+  },
+  {
+    "revision": "e5d2e2bb6fb7c2797c85",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/js/runtime-main.b1fb8441.js"
+  },
+  {
+    "revision": "a9a31672f191665cfd4573c57b0917ed",
+    "url": "/RED-KINNG/facerecognizerAPP.git/static/media/brain.a9a31672.png"
+  }
+]);
